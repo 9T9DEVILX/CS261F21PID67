8ef2094e69e3574b514244bd0f3352ab4718041c
AhmadArshad123 <ahmadjutt454979@gmail.com>
2021-11-03 16:38:47 +0500

    Login.ui to Login.py

--- LoginScreen.py
+++ LoginScreen.py
@@ -6,3 +6,67 @@
 #
 # WARNING! All changes made in this file will be lost!
 
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(789, 551)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setGeometry(QtCore.QRect(0, 0, 791, 531))
+        self.frame.setStyleSheet("background-color: rgb(255, 251, 233);\n"
+"")
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.label = QtWidgets.QLabel(self.frame)
+        self.label.setGeometry(QtCore.QRect(-30, -10, 541, 551))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.frame)
+        self.label_2.setGeometry(QtCore.QRect(450, 60, 231, 91))
+        self.label_2.setStyleSheet("font: 72pt \"Segoe Print\";")
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.frame)
+        self.label_3.setGeometry(QtCore.QRect(470, 150, 191, 31))
+        self.label_3.setStyleSheet("font: 75 12pt \"Tahoma\";\n"
+"font: 10pt \"MS Shell Dlg 2\";")
+        self.label_3.setObjectName("label_3")
+        self.pushButton = QtWidgets.QPushButton(self.frame)
+        self.pushButton.setGeometry(QtCore.QRect(470, 230, 211, 31))
+        self.pushButton.setStyleSheet("background-color: rgb(0, 0, 0);\n"
+"color: rgb(255, 251, 233);\n"
+"font: 10pt \"MS Shell Dlg 2\";")
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 789, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "<html><head/><body><p><img src=\":/newPrefix/WhatsApp Image 2021-10-23 at 10.21.13 PM.jpeg\"/><img src=\":/newPrefix/WhatsApp Image 2021-10-23 at 10.58.23 PM.jpeg\"/></p></body></html>"))
+        self.label_2.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:48pt; font-weight:600;\">M &amp; M</span></p></body></html>"))
+        self.label_3.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:14pt;\">Movies and Munching</span></p></body></html>"))
+        self.pushButton.setText(_translate("MainWindow", "Click here to Continue ----->"))
+
+import login_rc
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
